fb7ffd4d0fc07047c67beef2e596b824646eed65
Akshay <akshaykhanapuri12@gmail.com>
2019-01-07 22:59:34 +0530

    before adding all the info

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length = 255)
     pub_date = models.DateTimeField()
-    body = models.TextField()    
+    body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+
+    def summarize(self):
+        return self.body[:100]
+
+    def __str__(self):
+        return self.title
